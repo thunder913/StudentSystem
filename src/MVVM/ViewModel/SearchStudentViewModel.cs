cd9b949785ae3bc54268eff5bc4d97c0b135d6a2
thunder913 <andon_00@abv.bg>
2022-04-28 22:24:55 +0300

    added search page layout

--- src/MVVM/ViewModel/SearchStudentViewModel.cs
+++ src/MVVM/ViewModel/SearchStudentViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using StudentSystem.Core;
+using StudentSystem.MVVM.Model;
+using StudentSystem.MVVM.Model.DB;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +14,8 @@ namespace StudentSystem.MVVM.ViewModel
     {
         private IViewModel _currentViewModel;
         private IViewModel _currentViewModelParent;
+        public Student Student { get; set; }
+        public ObservableCollection<StudentSearchResult> StudentsResults { get; set; } = new ObservableCollection<StudentSearchResult>() { new StudentSearchResult();
         public IViewModel CurrentViewModel
         {
             get => _currentViewModel;
